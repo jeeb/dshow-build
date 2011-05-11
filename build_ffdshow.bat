7958d27d0d507dab0b7c671ff30c4b6d0fe02b62
Jan E <jeebjp@gmail.com>
2011-05-11 03:28:56 +0300

    Update stuff, and add a .gitignore that ignores the ffdshow-tryouts' gcc build log.

--- build_ffdshow.bat
+++ build_ffdshow.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 SET MSYS=C:\JEEB\msys\1.0
-SET MINGW32=C:\JEEB\msys\cross-mingw.gcc460.ml.20110411\i686-pc-mingw32
-SET MINGW64=C:\JEEB\msys\cross-mingw.gcc460.ml.20110411\x86_64-w64-mingw32
+SET MINGW32=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\i686-pc-mingw32
+SET MINGW64=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\x86_64-w64-mingw32
 SET PATH=%PATH%;%MINGW32%\bin;%MINGW64%\bin;%MSYS%\bin
 
 call "%VS100COMNTOOLS%vsvars32.bat"
@@ -11,4 +11,4 @@ cd ffdshow-tryouts
 
 devenv ffdshow_2010.sln /Rebuild "Release|Win32"
 
-MakeAll.bat
+MakeAll.bat 2> ..\catch_them_all.txt
