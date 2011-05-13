28e904b62478b6839127554d4a4037637c768be6
Jan E <jeebjp@gmail.com>
2011-05-13 12:08:14 +0300

    Update MPC-HC and ffdshow-tryouts

--- build_ffdshow.bat
+++ build_ffdshow.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
 SET MSYS=C:\JEEB\msys\1.0
-SET MINGW32=C:\JEEB\msys\tdm-452
-REM SET MINGW64=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\x86_64-w64-mingw32
-SET PATH=%PATH%;%MINGW32%\bin;%MSYS%\bin
+SET MINGW32=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\i686-pc-mingw32
+SET MINGW64=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\x86_64-w64-mingw32
+SET PATH=%PATH%;%MINGW32%\bin;%MINGW64%\bin;%MSYS%\bin
 
 call "%VS100COMNTOOLS%vsvars32.bat"
 
