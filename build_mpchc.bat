d9e16538e29cffa1645c85a6a769b020a6a6cdb6
Jan E <jeebjp@gmail.com>
2011-06-30 15:13:13 +0300

    Update stuff. Make mingws easier to be switched between.

--- build_mpchc.bat
+++ build_mpchc.bat
@@ -1,8 +1,5 @@
 REM Set some shit:
-SET MSYS=C:\JEEB\msys\1.0
-SET MINGW32=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\i686-pc-mingw32
-SET MINGW64=C:\JEEB\msys\cross-mingw.gcc460.ml.20110422\x86_64-w64-mingw32
-SET PATH=%PATH%;%MINGW32%\bin;%MINGW64%\bin;%MSYS%\bin
+call env_vars.bat
 
 cd mpc-hc
 rem build_2010.bat clean x86
---
+++ env_vars.bat
@@ -0,0 +1,6 @@
+REM Environment variables for your build system.
+
+SET MSYS=C:\JEEB\msys\1.0
+SET MINGW32=C:\JEEB\msys\cross-mingw.gcc453.ml.20110512\i686-pc-mingw32
+SET MINGW64=C:\JEEB\msys\cross-mingw.gcc453.ml.20110512\x86_64-w64-mingw32
+SET PATH=%PATH%;%MINGW32%\bin;%MINGW64%\bin;%MSYS%\bin
